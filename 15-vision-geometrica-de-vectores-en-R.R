b96be50fcaf072d447e0cffd8611949ca2ccc52b
Cesar Gerardo Hernández Vargas <66124391+cghv94@users.noreply.github.com>
2020-09-30 08:24:53 -0500

    Rename Vision geométrica de vectores en R.R to 15-vision-geometrica-de-vectores-en-R.R

--- 15-vision-geometrica-de-vectores-en-R.R
+++ 15-vision-geometrica-de-vectores-en-R.R
@@ -1,9 +1,9 @@
-# UNIVERSIDAD NACIONAL AUT”NOMA DE M…XICO
-# Facultad de EconomÌa
+# UNIVERSIDAD NACIONAL AUT√ìNOMA DE M√âXICO
+# Facultad de Econom√≠a
 # Taller III 2020-1
-# Profesor: Arturo MÈrida Monroy
-# Adjunto: Cesar Hern·ndez
-# PR¡CTICA 6: VISI”N GEOM…TRICA DE VERTORES EN R
+# Profesor: Arturo M√©rida Monroy
+# Adjunto: Cesar Hern√°ndez
+# PR√ÅCTICA 6: VISI√ìN GEOM√âTRICA DE VERTORES EN R
 
 library(dplyr)
 library(ggplot2)
@@ -103,7 +103,7 @@ grafica
 
 grafica + geom_abline(slope = .60)
 
-# Dibujando un polÌgono
+# Dibujando un pol√≠gono
 
 vx<-0:5
 tvx<-vx+5
