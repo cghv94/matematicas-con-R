bf53f7937e7c1ad2661b0245d9b9ae7ed8a04a5c
Cesar Gerardo Hernández Vargas <66124391+cghv94@users.noreply.github.com>
2020-09-30 12:46:15 -0500

    Update 14-operaciones-con-matrices-en-R.R

--- 14-operaciones-con-matrices-en-R.R
+++ 14-operaciones-con-matrices-en-R.R
@@ -1,8 +1,7 @@
 # UNIVERSIDAD NACIONAL AUTÓNOMA DE MÉXICO
 # Facultad de Economía
-# Taller III 2020-1
-# Profesor: Arturo Mérida Monroy
-# Adjunto: Cesar Hernández
+# Taller III 2021-1
+# Profesor: Cesar Hernández
 # PRÁCTICA 5: OPERACIONES CON MATRICES EN R
 
 # Borrar objetos y variables
